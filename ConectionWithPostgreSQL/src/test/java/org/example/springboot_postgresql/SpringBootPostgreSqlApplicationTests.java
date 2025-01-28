af26e75fd80f217b34d9973d8980ab4857450074
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-01-28 23:27:34 +0100

    change the name module

---
+++ ConectionWithPostgreSQL/src/test/java/org/example/springboot_postgresql/SpringBootPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgreSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
