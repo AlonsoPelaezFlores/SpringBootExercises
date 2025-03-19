c3d5d2c3486d8f2cb18d68b5e3e05490e8937882
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-03-20 00:26:27 +0100

    Eliminacion de UserResponse.java Mejora de la configuracion de Spring Security. Se ha configurado correctamente las rutas, las sesiones http y el maximo de sesiones por usuario.

--- AuthenticationSystem/src/main/java/com/example/authenticationsystem/config/SecurityConfig.java
+++ AuthenticationSystem/src/main/java/com/example/authenticationsystem/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.authenticationsystem.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,15 +16,23 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login").permitAll()
-                        .requestMatchers("/auth/register").permitAll()
-                        .requestMatchers("/auth").permitAll()
+                        .requestMatchers(
+                                "/auth/login",
+                                "/auth/register",
+                                "/auth").permitAll()
+                        .requestMatchers(
+                                "/auth/check-session",
+                                "auth/logout").authenticated()
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
-                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessHandler((req, res, auth) -> res.setStatus(200)))
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessHandler((req, res, auth) -> res.setStatus(200))
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true))
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1));
         return http.build();
     }
 }
